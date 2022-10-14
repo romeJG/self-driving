d25b496c0c50f00f6561b69c57b2ff1a0a0008bb
jastineee <201810083@fit.edu.ph>
2022-10-14 21:33:30 +0800

    car steering update
    - car will not turn if speed is 0
    - car will flip the values of the left and right if going backwards

--- controls.js
+++ controls.js
@@ -28,7 +28,6 @@ class Controls {
                     this.reverse = true
                     break
             }
-            console.table(this)
         }
         document.onkeyup = (event) => {
             switch (event.key) {
@@ -45,7 +44,6 @@ class Controls {
                     this.reverse = false
                     break
             }
-            console.table(this)
         }
     }
 }
