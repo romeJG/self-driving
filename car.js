521768c077f5a3ff296b93c28d8a3be99397baf6
jastineee <201810083@fit.edu.ph>
2022-10-15 10:01:04 +0800

    rays colision detection - the closes part of the ray that will colide with the car will recolor to black

--- car.js
+++ car.js
@@ -20,9 +20,9 @@ class Car {
         this.controls = new Controls()
     }
 
-    update() {
+    update(roadBorders) {
         this.#move()
-        this.sensor.update()
+        this.sensor.update(roadBorders)
     }
 
     #move() {
